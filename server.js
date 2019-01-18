e2a212ec5313005da25421725dfec1813c321a14
Poydev Developer <poydevelop@gmail.com>
2019-01-18 10:10:24 +0700

    response JSON

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var http = require('http');
 var fs = require('fs');
+var params = {
+	"name" : "poydev",
+	"job" : "programmer",
+	"age" : 30
+};
+
 
 http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type':'text/html'});
-	var myStream = fs.createReadStream(__dirname+"/"+'index.html','utf8');
-	myStream.pipe(res);
-}).listen(8081,'127.0.0.1');
+	//res.writeHead(200, {'Content-Type':'text/html'});
+	//var myStream = fs.createReadStream(__dirname+"/"+'index.html','utf8');
+	//myStream.pipe(res);
+	res.writeHead(200, {'Content-Type':'application/json'});
+	res.end(JSON.stringify(params));
+}).listen(3000,'127.0.0.1');
